95986d465b56abf316151e537ea922639c4b0b99
Mateo Bellini <bellinimateo872@gmail.com>
2023-06-15 18:24:43 -0300

    Linkeo de componentes hecho

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-import '../styles/callToAction.css';
+import "../styles/callToAction.css";
 import Carousel from "./carousel";
-
+import { Link } from "react-router-dom";
 
 function CallToAction() {
-    return (
-        <>
-            <div>
-                <div className="card mx-auto p-2">
-                    <img src="./images/thinh-n.jpg" className="card-img-top" alt="..." />
-                    <div className="card-body text-center shadow-lg p-3 mb-5 bg-body-tertiary rounded">
-                        <h5 className="card-title">MyTinerary</h5>
-                        <p className="card-text">Find your perfect trip, designed by insiders who know and love their cities!.</p>
-                        <a href="#" className="btn btn-secondary shadow-sm">Get started</a>
-                    </div>
-                </div>
-            </div>
-            <h1 className="text-center lead">Popular Mytineraries</h1>
-            <Carousel />
-        </>
-    )
+  return (
+    <>
+      <div>
+        <div className="card mx-auto p-2">
+          <img src="./images/thinh-n.jpg" className="card-img-top" alt="..." />
+          <div className="card-body text-center shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+            <h5 className="card-title">MyTinerary</h5>
+            <p className="card-text">
+              Find your perfect trip, designed by insiders who know and love
+              their cities!.
+            </p>
+            <Link to="/cities" className="btn btn-secondary shadow-sm">
+              Get started
+            </Link>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-center lead">Popular Mytineraries</h1>
+      <Carousel />
+    </>
+  );
 }
 
 export default CallToAction;
