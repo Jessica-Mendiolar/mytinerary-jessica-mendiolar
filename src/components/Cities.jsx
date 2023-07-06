284acf4a9b1c94a2d7ef972d0b707afb5e6bc182
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-06 13:40:28 -0300

    Search hecho

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,43 +1,73 @@
-import { useEffect, useState } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../styles/cities.css";
 import { BsFillGeoFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import "../styles/cities.css";
 
 function Cities() {
   const [cities, setCities] = useState([]);
-  let citiesDb;
+  const [searchTerm, setSearchTerm] = useState("");
+
   async function getData() {
-    citiesDb = await axios.get(
+    const response = await axios.get(
       "https://api-cities-crud-xh50.onrender.com/api/cities"
     );
-    setCities(citiesDb.data.response.cities);
+    setCities(response.data.response.cities);
   }
+
   useEffect(() => {
     getData();
   }, []);
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCities = cities.filter((city) => {
+    return city.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  const renderCities = filteredCities.length > 0 ? filteredCities : null;
+
   return (
     <>
       <h1 className="title text-center lead mb-0">Cities</h1>
-      <div className="container_city d-flex flex-wrap justify-content-center justify-content-md-between my-4">
-        {cities?.map((city) => (
-          <Link
-            to={`/cityDetails/${city._id}`}
-            className="city d-flex flex-column justify-content-end p-3"
-            style={{
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${city.image})`,
-            }}
-            key={city._id}
-          >
-            <h4 className="mb-0">{city.name}</h4>
-            <div className="point d-flex align-items-center gap-1">
-              <BsFillGeoFill className="mb-0" />
-              <p className="mb-0">{city.country}</p>
-            </div>
-          </Link>
-        ))}
+      <div className="input_container d-flex justify-content-end mx-auto">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <div
+        className={`container_city d-flex flex-wrap justify-content-center my-4 ${
+          !renderCities
+            ? "justify-content-center"
+            : "justify-content-md-between"
+        }`}
+      >
+        {renderCities ? (
+          renderCities.map((city) => (
+            <Link
+              to={`/cityDetails/${city._id}`}
+              className="city d-flex flex-column justify-content-end p-3"
+              style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${city.image})`,
+              }}
+              key={city._id}
+            >
+              <h4 className="mb-0">{city.name}</h4>
+              <div className="point d-flex align-items-center gap-1">
+                <BsFillGeoFill className="mb-0" />
+                <p className="mb-0">{city.country}</p>
+              </div>
+            </Link>
+          ))
+        ) : (
+          <p>No se encontraron ciudades</p>
+        )}
       </div>
     </>
   );
